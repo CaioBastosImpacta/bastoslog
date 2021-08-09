0eda927866c0420db98818214be5349ce9aac866
CaioBastosImpacta <caio.bastos@aluno.faculdadeimpacta.com.br>
2021-08-09 18:48:43 -0300

    criando o pojeto de logistica

---
+++ src/main/java/com/bsworks/bastoslog/BastoslogApiApplication.java
@@ -0,0 +1,13 @@
+package com.bsworks.bastoslog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BastoslogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BastoslogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bsworks/bastoslog/BastoslogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsworks.bastoslog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BastoslogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
